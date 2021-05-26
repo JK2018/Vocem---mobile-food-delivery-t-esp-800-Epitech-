eef5e6ff08c49c5ccca37a20a13566d3dbcb0594
Fabrice Sumsa <fabrice.sumsa@epitech.eu>
2021-05-27 00:37:18 +0200

    add pagination page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vocem/routes/pagination.dart';
 
 import 'screens/screens_index.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: Auth(),
+      home: Pagination(),
     );
   }
 }
---
+++ lib/routes/pagination.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:vocem/screens/screens_index.dart';
+
+class Pagination extends StatefulWidget {
+  @override
+  _PaginationState createState() => _PaginationState();
+}
+
+class _PaginationState extends State<Pagination> {
+  int _currentIndex = 0;
+
+  final tabs = [
+    Center(child: HomePage()),
+    Center(child: Text("2")),
+    Center(child: Text("3")),
+    Center(child: Text("4")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text("Test"),
+      //   backgroundColor: Colors.deepPurple,
+      // ),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+           BottomNavigationBarItem(
+             icon: Icon(Icons.home_outlined),
+             label: "Home",
+             backgroundColor: Colors.deepPurple,
+           ),
+           BottomNavigationBarItem(
+             icon: Icon(Icons.search),
+             label: "Search",
+             backgroundColor: Colors.deepPurple,
+           ),
+           BottomNavigationBarItem(
+             icon: Icon(Icons.list_alt_outlined),
+             label: "Commandes",
+             backgroundColor: Colors.deepPurple
+           ),
+           BottomNavigationBarItem(
+             icon: Icon(Icons.account_circle),
+             label: "Profil",
+             backgroundColor: Colors.deepPurple
+           ),
+        ],
+        onTap: (index){
+          setState(() {
+            _currentIndex= index;
+          });
+        },
+      ),
+    );
+  }
+}
